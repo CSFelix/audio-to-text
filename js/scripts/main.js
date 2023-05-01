0c2d1c7613b567f7360ed972ddf56408f8cfef62
CSFelix <57205604+CSFelix@users.noreply.github.com>
2023-04-30 21:42:36 -0300

    Commit

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -3,8 +3,17 @@
 	**	Elements **
 	***************
 */
-const micStatus = ['mic', 'settings_value', 'mic_off'];
+const micStatus = ['mic', 'settings_voice', 'mic_off'];
 const recordButton = document.getElementById('recordButton');
+const cleanTextButton = document.getElementById('cleanTextButton');
+
+const extractedTextField = document.getElementById('extractedTextField');
+const languageSelector = document.getElementById('languageSelector');
+const copyExtractedText = document.getElementById('copyExtractedText');
+
+let recognizingFlag = false;
+let finalTranscript = '';
+let recognition;
 
 
 
@@ -13,35 +22,47 @@ const recordButton = document.getElementById('recordButton');
 	** Event Listeners **
 	*********************
 */
+
 recordButton.addEventListener('click', () => {
 	// if 'Speech Recognition API' is supported by the browser,
 	// the audio permission is requested and, if granted, the audio
 	// recognition and convertion processes are started.
 	if ('webkitSpeechRecognition' in window) {
-		const recognition = new webkitSpeechRecognition();
-		recognition.continuous = true;
-		recognition.interimResults = true;
 
-		recognition.onresult = function(event) {
-		  let interim_transcript = '';
-		  let final_transcript = '';
+		// If audio is not being recognized, kick off recognizition
+		if (!recognizingFlag) {
+			recognizingFlag = true;
 
-		  for (let i = event.resultIndex; i < event.results.length; ++i) {
-		    if (event.results[i].isFinal) {
-		      final_transcript += event.results[i][0].transcript;
-		    } else {
-		      interim_transcript += event.results[i][0].transcript;
-		    }
-		  }
-		  // final_transcript = capitalize(final_transcript);
-		  // final_span.innerHTML = linebreak(final_transcript);
-		  // interim_span.innerHTML = linebreak(interim_transcript);
+			recognition = new webkitSpeechRecognition();
+			recognition.continuous = true;
+			recognition.interimResults = false;
+			recognition.lang = languageSelector.value;
 
-		  console.log('Interim Transcript:', interim_transcript);
-		  console.log('Final Transcript:', final_transcript);
-		};
+			
+			recognition.onresult = (event) => {
+				for (let index = event.resultIndex; index < event.results.length; ++index) {
+					if (event.results[index].isFinal) {
+						finalTranscript += event.results[index][0].transcript;
+					}
+				}
 
-		recognition.start();
+				extractedTextField.innerHTML = finalTranscript;
+			};
+
+			
+			recognition.start();
+			recordButton.innerHTML = micStatus[1];
+			recordButton.classList.add('recordButtonRecordingStatus');
+		}
+
+		// If audio is being recognized, stop recognizition
+		else {
+			recognizingFlag = false;
+			recognition.stop();
+
+			recordButton.innerHTML = micStatus[0];
+			recordButton.classList.remove('recordButtonRecordingStatus');
+		}
 	}
 
 	// if 'Speech Recognition API' is not supported by the browser,
@@ -54,5 +75,52 @@ recordButton.addEventListener('click', () => {
       		icon: "./assets/logo.avif",
       		timeout: 3500
     	});
+
+    	recordButton.innerHTML = micStatus[2];
 	}
 });
+
+copyExtractedText.addEventListener('click', () => {
+
+	// If there are no extracted text, a toast message is thrown
+	if (extractedTextField.innerHTML === '') {
+		VanillaToasts.create({
+      		title: "No Extracted Text To Copy",
+      		text: "There are no extracted text to copy. Try to record an audio before trying to copy a text!",
+      		type: "warning", // warning, error, info, success
+      		icon: "./assets/logo.avif",
+      		timeout: 3500
+    	});
+	}
+
+	// If the audio is still being recognized, a toast message
+	// is thrown
+	else if (recognizingFlag) {
+		VanillaToasts.create({
+      		title: "Audio Is Still Being Recognized",
+      		text: "Stop the audio recognition before trying to copy the result text!",
+      		type: "warning", // warning, error, info, success
+      		icon: "./assets/logo.avif",
+      		timeout: 3500
+    	});
+	}
+
+	// If there are extracted text, the text is copied
+	// to the client clipboard
+	else {
+		navigator.clipboard.writeText(extractedTextField.innerHTML);
+
+		VanillaToasts.create({
+      		title: "Copied Text",
+      		text: "Copied text to the clipboard!",
+      		type: "success", // warning, error, info, success
+      		icon: "./assets/logo.avif",
+      		timeout: 3500
+    	});
+	}
+});
+
+cleanTextButton.addEventListener('click', () => {
+	finalTranscript = '';
+	extractedTextField.innerHTML = finalTranscript;
+})
